9802b0df994be9bdfc9887f2fe94794b27790fc4
Ralence <ranko.ostojic@gmail.com>
2019-12-21 14:04:33 +0100

    feat: add basic tabs navigation

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -3,11 +3,12 @@ import { Platform } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createDrawerNavigator } from 'react-navigation-drawer';
-import { createBottomTabsNavigator } from 'react-navigation-tabs';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailScreen from '../screens/MealDetailsScreen';
+import FavoritesScreen from '../screens/FavoritesScreen';
 
 import Colors from '../constants/colors';
 
@@ -27,11 +28,16 @@ const MealsNavigator = createStackNavigator({
             backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
         },
         headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
-
-
-
-
     }
 });
 
-export default createAppContainer(MealsNavigator);
+
+const MealsTabNavigator = createBottomTabNavigator({
+    Meals: {
+        screen: MealsNavigator
+    },
+    Favorites: FavoritesScreen
+
+});
+
+export default createAppContainer(MealsTabNavigator);
