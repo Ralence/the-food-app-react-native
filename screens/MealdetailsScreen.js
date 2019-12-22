29f8fb72d70c34fea0d7be9f0b6a6314cba586b0
Ralence <ranko.ostojic@gmail.com>
2019-12-22 18:57:52 +0100

    feat: add mealDetail screen content

--- screens/MealdetailsScreen.js
+++ screens/MealdetailsScreen.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { SafeAreaView, ScrollView, Image, View, Text, StyleSheet } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import HeaderButton from '../components/HeaderButton';
+import DefaultText from '../components/DefaultText';
+
+const ListItem = props => {
+    return <View style={styles.listItem}>
+        <DefaultText>{props.children}</DefaultText>
+    </View>
+}
 
 const MealDetailsScreen = props => {
-    const meal = props.navigation.getParam('item')
+    const meal = props.navigation.getParam('item');
+
     return (
-        <View style={styles.screen}>
-            <Text>{meal.title}</Text>
-        </View>
+        <ScrollView>
+            <Image source={{ uri: meal.imageUrl }} style={styles.image} />
+            <View style={styles.details}>
+                <DefaultText>{meal.duration}m</DefaultText>
+                <DefaultText>{meal.complexity.toUpperCase()}</DefaultText>
+                <DefaultText>{meal.affordability.toUpperCase()}</DefaultText>
+            </View>
+            <Text style={styles.title}>Ingredients</Text>
+            {meal.ingredients.map(ingredient => <ListItem key={ingredient}>{ingredient}</ListItem>)}
+            <Text style={styles.title}>Steps</Text>
+            {meal.steps.map(step => <ListItem key={step}>{step}</ListItem>)}
+        </ScrollView>
+
     )
 };
 
@@ -24,10 +42,26 @@ MealDetailsScreen.navigationOptions = navigationData => {
 };
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+    image: {
+        width: '100%',
+        height: 200
+    },
+    details: {
+        flexDirection: 'row',
+        padding: 15,
+        justifyContent: 'space-around'
+    },
+    title: {
+        fontFamily: 'open-sans-bold',
+        fontSize: 22,
+        textAlign: 'center'
+    },
+    listItem: {
+        marginVertical: 10,
+        marginHorizontal: 20,
+        borderColor: '#ccc',
+        borderWidth: 1,
+        padding: 10
     }
 });
 
